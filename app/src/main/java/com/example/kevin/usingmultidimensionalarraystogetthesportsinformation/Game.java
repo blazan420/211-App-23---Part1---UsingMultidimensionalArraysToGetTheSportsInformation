72d7c4cf8aa0414045bb290b9baba8b9b48fee89
kevin <kevinjobrien420@gmail.com>
2018-06-19 01:38:25 -0500

    212 App 23 - Part2 - Using Multidimensional Arrays to get the Sports Information. //coding

--- app/src/main/java/com/example/kevin/usingmultidimensionalarraystogetthesportsinformation/Game.java
+++ app/src/main/java/com/example/kevin/usingmultidimensionalarraystogetthesportsinformation/Game.java
@@ -17,4 +17,48 @@ public class Game {
         this.gameName = gameName;
     }
 
+    public String getGameName() {
+        return gameName;
+    }
+
+    public int getMinimumScore() {
+
+        int lowestScore = scores[0][0]; // Assume this value is the Smallest Score Value
+        for (int[] gameScores : scores) {
+            for (int score : gameScores) {
+                if (score < lowestScore) {
+                    lowestScore = score;
+                }
+            }
+        }
+        return lowestScore;
+    }
+
+    public int getMaximumScore() {
+
+        int highestScore = scores[0][0]; // Assume this value us the Highest Score Value
+
+        for (int[] gameScores : scores) {
+            for (int score : gameScores) {
+                if (score > highestScore) {
+                    highestScore = score;
+                }
+            }
+
+        }
+        return  highestScore;
+    }
+
+    public double getTheAverageValueOfScore(int[] scores) {
+
+        int total = 0;
+
+        //Sum Scores for the game
+        for (int score : scores) {
+            total += score;
+            total = total + score;
+        }
+        return (double) total / scores.length;
+    }
+
 }
